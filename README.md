0ca44c2d9f0ea54f143c785da0175c63926b8d5e
Nathan Osman <nathan@quickmediasolutions.com>
2024-08-20 21:54:58 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+## dnslive
+
+TODO
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "dnslive",
+		Usage: "DNS server for dynamic IP addresses",
+		Action: func(c *cli.Context) error {
+
+			// Wait for SIGINT or SIGTERM
+			sigChan := make(chan os.Signal, 1)
+			signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+			<-sigChan
+
+			return nil
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "fatal: %s\n", err.Error())
+	}
+}
